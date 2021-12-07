e40bf447964206ccda8bbca3971b35c767aaef65
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-07 18:59:38 +0900

    [Update] String => Char

--- SuitableMold.java
+++ SuitableMold.java
@@ -1,7 +1,7 @@
 public class SuitableMold{
   public static void main(String[] args){
     boolean a = true;
-    String b = "駆"; 
+    Char b = "駆"; //文字一つの場合、StringではなくChar
     double c = 3.14;
     long d = 314159265L;
     String e = "ミナトの攻撃!敵に15ポイントのダメージを与えた。";
