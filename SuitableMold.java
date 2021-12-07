07b98b7bfa66a87423b1d03107ccc92a3cb1a4a1
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-07 18:57:13 +0900

    first commit

---
+++ SuitableMold.java
@@ -0,0 +1,11 @@
+public class SuitableMold{
+  public static void main(String[] args){
+    boolean a = true;
+    String b = "駆"; 
+    double c = 3.14;
+    long d = 314159265L;
+    String e = "ミナトの攻撃!敵に15ポイントのダメージを与えた。";
+
+    System.out.println(d + e);
+  }
+}
