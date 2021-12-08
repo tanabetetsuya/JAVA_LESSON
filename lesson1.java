b526937440cb369868ea747454106411d9c2cd46
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-08 14:06:51 +0900

    [Add] if分の処理の追加

--- lesson1.java
+++ lesson1.java
@@ -1,6 +1,17 @@
+import java.util.Random;
+
 public class Lesson1{
   public static void main(String[] args){
-    int isHungry = 0;
-    
+    Random random = new Random();
+    int isHungry = random.nextInt(2);
+    String food = "ラーメン";
+    System.out.println("こんにちは");
+    if (isHungry == 0){
+      System.out.println("お腹がいっぱいです。");
+    }else{
+      System.out.println("腹ペコです。");
+      System.out.println(food + "をいただきます。");
+    }
+      System.out.println("ご馳走様でした。");
   }
 }
