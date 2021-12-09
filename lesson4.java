e0a37eb2db4f602722b9d51692021d0d14b7138b
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-09 22:36:35 +0900

    [first commit]

---
+++ lesson4.java
@@ -0,0 +1,13 @@
+public class Lesson4{
+  public static void introduceOneself(){
+    String name = "田辺";
+    int age = 27;
+    float height = 168;
+    String zodiac = "亥";
+    System.out.println("私の名前は"+name+"です。年齢は"+age+"です。身長は"+height+"です。干支は"+zodiac+"です。");
+  }
+
+  public static void main(String[] args){
+    introduceOneself();
+  }
+}
