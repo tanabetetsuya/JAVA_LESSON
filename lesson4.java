b370472fcf8c8cb8f112fb6d17558a7948009d0e
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-10 08:25:54 +0900

    [Add] emailメソッドの定義

--- lesson4.java
+++ lesson4.java
@@ -5,6 +5,13 @@ public class Lesson4{
     float height = 168;
     String zodiac = "亥";
     System.out.println("私の名前は"+name+"です。年齢は"+age+"です。身長は"+height+"です。干支は"+zodiac+"です。");
+    email("面接日程調整", "test@test.com", "希望日程は12/1です。");
+  }
+
+  public static void email(String title, String address, String text){
+    System.out.println(address+"に、以下のメールを送信しました");
+    System.out.println("件名:"+title);
+    System.out.println("本文:"+text);
   }
 
   public static void main(String[] args){
