52d86d8bef0203c4276e40613d06c4fd4f4d9ff7
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-10 21:35:33 +0900

    [Update]メソッドの追加

--- Hero.java
+++ Hero.java
@@ -1,4 +1,14 @@
 public class Hero{
   String name;
   int hp;
+
+  public void sleep(){
+    this.hp = 100; //自分自身のhpフィールド
+    System.out.println(this.name+"は、眠って回復した!"); //this.name => 自分自身のnameフィールド
+  }
+  public void sit(int sec){
+    this.hp += sec; //座った秒数分、体力を回復
+    System.out.println(this.name+"は、"+sec+"秒座った!");
+    System.out.println(this.name+"は、"+sec+"HP回復した!");
+  }
 }
