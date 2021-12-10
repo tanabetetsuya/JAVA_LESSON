72aefdf89aa3582e7e17434b3ab9728db3d3870b
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-10 19:32:23 +0900

    [Update] Thread.sleepの追加

--- Kouhan.java
+++ Kouhan.java
@@ -8,6 +8,7 @@ public class Kouhan{
   public static void showMondokoro() throws Exception {
     System.out.println("飛車さん、角さん。");
     System.out.println("この紋所が目に入らぬか");
+    Thread.sleep(3000);//３秒間の停止時間を追加
     comment.Zenhan.doTogame();
   }
 }
