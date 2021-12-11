b1fa8e4ea24aca6a3cf68e1cf39d68bf89b1fad2
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-11 17:13:00 +0900

    [first commit]

---
+++ Lesson5/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+  public static void heal(int hp){
+    hp += 10;
+  }
+  public static void heal(Thief thief){
+    thief.hp += 10;
+  }
+
+  public static void main(String[] args){
+    int baseHP = 25;
+    Thief t = new Thief("アサカ", 35);
+    System.out.println(baseHP+ ":" + t.hp);
+    heal(baseHP);
+    heal(t);
+    System.out.println(baseHP+":"+t.hp);
+  }
+}
