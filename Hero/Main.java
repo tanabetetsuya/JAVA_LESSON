4d71bc4b9b32ffec4c93a320fac6f2a1f8e5e499
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-11 13:13:42 +0900

    [Update] コメントアウトや操作の新しい記述

--- Hero/Main.java
+++ Hero/Main.java
@@ -3,11 +3,11 @@ public class Main{
     Hero h = new Hero();
     h.name = "ミナト";
     h.hp = 100;
-    System.out.println("勇者"+h.name+"を生み出しました。");
-    h.sit(5);
-    h.slip();
-    h.sit(25);
-    h.run();
+    //System.out.println("勇者"+h.name+"を生み出しました。");
+    //h.sit(5);
+    //h.slip();
+    //h.sit(25);
+    //h.run();
 
     Matango m1 = new Matango();
     m1.hp = 50;
@@ -18,10 +18,10 @@ public class Main{
     m2.suffix = 'B';
 
     //冒険の始まり
-    h.slip();
-    m1.run();
-    m2.run();
-    h.run();
+    //h.slip();
+    //m1.run();
+    //m2.run();
+    //h.run();
 
     Sword s = new Sword();
     s.name = "炎の剣";
@@ -29,5 +29,18 @@ public class Main{
     h.sword = s; //swordフィールドに生成済みの剣インスタンスを代入
     System.out.println("現在の武器は"+ h.sword.name);
     h.attack();
+
+    Hero h1 = new Hero();
+    h1.name = "カイト";
+    h1.hp = 100;
+    Hero h2 = new Hero();
+    h2.name = "シズク";
+    h2.hp = 150;
+    Wizard w = new Wizard();
+    w.name = "ウィザード";
+    w.hp = 50;
+    w.heal(h1);
+    w.heal(h2);
+    w.heal(h2);
   }
 }
