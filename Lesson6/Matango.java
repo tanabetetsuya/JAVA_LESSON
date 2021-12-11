aa54115e8d5d6a8de599d08a0b19edd86c6d281e
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-11 20:49:04 +0900

    [first commit]

---
+++ Lesson6/Matango.java
@@ -0,0 +1,18 @@
+public class Matango{
+  int hp = 50;
+  final int Level = 10; //フィールドLevelは10で固定
+  char suffix;
+
+  public Matango(char suffix){
+    this.suffix = suffix;
+  }
+
+  public void attack(Hero h){
+    System.out.println("キノコ"+this.suffix+"の攻撃");
+    h.hp -= 10;
+  }
+  
+  public void run(){
+    System.out.println("おばけキノコ"+this.suffix+"は逃げ出した!");
+  }
+}
---
+++ Lesson6/PoisonMatango.java
@@ -0,0 +1,6 @@
+public class PoisonMatango extends Matango{
+  int attack_number = 5;
+  public PoisonMatango(char suffix){
+    super();
+  }
+}
