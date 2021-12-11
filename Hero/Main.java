b935b9eae517cbf162075db1c6f312b569bcfb88
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-11 12:45:48 +0900

    [Update] Mainクラス内、ダブルクォーテーションかシングルへの変更

--- Hero/Main.java
+++ Hero/Main.java
@@ -11,17 +11,18 @@ public class Main{
 
     Matango m1 = new Matango();
     m1.hp = 50;
-    m1.suffix = "A";
+    m1.suffix = 'A';
 
     Matango m2 = new Matango();
     m2.hp = 48;
-    m2.suffix = "B";
+    m2.suffix = 'B';
 
     //冒険の始まり
     h.slip();
     m1.run();
     m2.run();
     h.run();
+
     Sword s = new Sword();
     s.name = "炎の剣";
     s.damage = 10;
