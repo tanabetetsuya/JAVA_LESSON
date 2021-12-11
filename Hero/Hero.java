5e9b91bb7854d87d5dcd590cca8bcbd9c812bbe2
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-11 09:01:39 +0900

    [first commit]

---
+++ Hero/Hero.java
@@ -0,0 +1,24 @@
+public class Hero{
+  String name;
+  int hp;
+
+  public void sleep(){
+    this.hp = 100; //自分自身のhpフィールド
+    System.out.println(this.name+"は、眠って回復した!"); //this.name => 自分自身のnameフィールド
+  }
+  public void sit(int sec){
+    this.hp += sec; //座った秒数分、体力を回復
+    System.out.println(this.name+"は、"+sec+"秒座った!");
+    System.out.println(this.name+"は、"+sec+"HP回復した!");
+  }
+  public void slip(){
+    this.hp -= 5;
+    System.out.println(this.name+"は、転んだ!");
+    System.out.println("-5のダメージ");
+  }
+  public void run(){
+    System.out.println(this.name+"は逃げ出した!");
+    System.out.println("gameover");
+    System.out.println("最終HPは"+this.hp+"でした。");
+  }
+}
