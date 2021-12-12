030beb4913a4a9667220fb18676fa641b5bc273c
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-12 13:13:57 +0900

    [first commit]

---
+++ Lesson7/TangibleAsset.java
@@ -0,0 +1,12 @@
+public abstract class TangibleAsset{
+  String name;
+  int price;
+  String color;
+
+
+//抽象メソッドの定義
+public abstract String getName();
+public abstract int getPrice();
+public abstract String getColor();
+
+}
