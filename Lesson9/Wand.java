dd845328af0cb577a8e4a90cfe6b21a8df837616
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-12 22:34:17 +0900

    [Finish]

--- Lesson9/Wand.java
+++ Lesson9/Wand.java
@@ -1,12 +1,30 @@
 public class Wand{
   private String name;
   private double power;
-  
+
   public Wand(String name, double power){
     this.name = name;
     this.power = power;
   }
   public double getPower(){
     return this.power;
+  }
+  public String getName(){
+    return this.name;
+  }
+
+  public void setPower(double power){
+    if(this.power < 0.5 && this.power > 100){
+      throw new IllegalArgumentException
+      ("処理を中断");
+    }
+    this.power = power;
+  }
+  public void setName(String name){
+    if(this.name.length() < 3){
+      throw new IllegalArgumentException
+      ("処理を中断");
+    }
+    this.name = name;
   }
 }
--- Lesson9/Wizard.java
+++ Lesson9/Wizard.java
@@ -2,13 +2,53 @@ public class Wizard{
   private int hp;
   private int mp;
   private String name;
-  Wand w = new Wand("魔法の杖", 50);
+  private Wand wand;
+  Wand wa = new Wand("魔法の杖", 50);
+
+  
+  public String getName(){
+    return this.name;
+  }
+  public void setName(String name){
+    if(this.name.length() < 3){
+      throw new IllegalArgumentException
+      ("処理を中断");
+    }
+      this.name = name;
+  }
+  public int getHp(){
+    return this.hp;
+  }
+  public void setHp(int hp){
+    if(this.hp >= 0){
+      this.hp = hp;
+    }else{
+      this.hp = 0;
+    }
+  }
+  public int getMp(){
+    return this.mp;
+  }
+  public void setMp(int mp){
+    if(this.mp >= 0){
+      throw new IllegalArgumentException
+      ("処理を中断");
+    }
+      this.hp = hp;
+  }
+  //魔法使いが杖を持つメソッドを追加
+  public Wand getWand(){
+    return this.wand;
+  }
+  public void setWand(Wand wand){
+    this.wand = wand;
+  }
 
   public void heal(Hero h){
     int basePoint = 10;
-    int recovPoint = (int)(basePoint * this.w.getPower());
-    h.setHp(h.getHp() + recovPoint);
-    System.out.println(h.getName() + "のHPを" + recovPoint + "回復した");
+    int recoverPoint = (int)(basePoint*wa.getPower());
+    h.setHp(h.getHp() + recoverPoint);
+    System.out.println(h.getName() + "のHPを" + recoverPoint + "回復した");
     System.out.println(h.getName()+"のHPは"+h.getHp());
   }
 }
