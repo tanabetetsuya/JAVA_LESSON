8268cc79f8c52b8bb00f00ca87eeefabe9be8590
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-13 22:27:51 +0900

    [first commit]

---
+++ HIGH_LOW_GAME/Deck.java
@@ -0,0 +1,18 @@
+public class Deck{
+
+  final int CARD_TOTAL = 52;
+  static int card_list;
+
+  public Deck(){
+    int[] card_list = new int[52];
+      for(int j=0;j<=51;j++){
+          card_list[j] = j;
+          i++;
+      }
+  }
+
+  public static void main(String[] args){
+
+    
+  }
+}
