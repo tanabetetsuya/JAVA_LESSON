929516745ae692897d463fcb191e5a652c7b457b
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-25 18:47:01 +0900

    [Add] 文字列の連結メソッドの作成

---
+++ Lesson11/Combine.java
@@ -0,0 +1,5 @@
+public class Combine{
+  public Combine(String file, String folder){
+    System.out.println(folder + file);
+  }
+}
--- Lesson11/Main.java
+++ Lesson11/Main.java
@@ -1,14 +1,12 @@
 public class Main{
   public static void main(String[] args){
+    String file = "readme.txt";
+    String folder = "c:¥user¥";
+    Combine com = new Combine(file, folder);
     StringBuilder sb = new StringBuilder();
     for(int i=1;i<101;i++){
       sb.append(i+",");
     }
-    String s = sb.toString();
-    System.out.println(s);
-    String[] a = s.split(",");
-    for(String aa : a){
-      System.out.println(aa);
-    }
+
   }
 }
