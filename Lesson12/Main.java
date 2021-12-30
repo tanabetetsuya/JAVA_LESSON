97cbb65252b5716bb7483cfcc7ec4c711b849eb7
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-30 20:55:13 +0900

    [Add] Heroクラスの追加

--- Lesson12/Main.java
+++ Lesson12/Main.java
@@ -25,3 +25,12 @@ public class Main{
     }
   }
 }
+public class Hero{
+  private String name;
+  public Hero(String name){
+    this.name = name;
+  }
+  public String getName(){
+    return this.name;
+  }
+}
