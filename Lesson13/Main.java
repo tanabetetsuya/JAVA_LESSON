542fc7d2f8824e82e29e9f500c5392f3122b462b
tanabetetsuya <81751235+tanabetetsuya@users.noreply.github.com>
2021-12-31 20:31:34 +0900

    [Add]例外処理の追加

--- Lesson13/Main.java
+++ Lesson13/Main.java
@@ -1,7 +1,13 @@
 public class Main{
   public static void main(String[] args){
-    String s = null;
-    s.length();
-    
+    try{
+      String s = null;
+      System.out.println(s.length());
+    }catch(Exception e){
+      System.out.println("NullPointerException例外をcatchしました");
+      System.out.println("--スタックトレースを表示する");
+      e.printStackTrace();
+      System.out.println("--スタックトレースはここまで");
+    }
   }
 }
